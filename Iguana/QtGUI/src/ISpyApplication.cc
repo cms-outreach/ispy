5d9c76e93a2e3137e9c518fb2bf4a6002356392d
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-08-17 14:11:24 +0200

    Updated to HEAD

--- Iguana/QtGUI/src/ISpyApplication.cc
+++ Iguana/QtGUI/src/ISpyApplication.cc
@@ -4164,7 +4164,7 @@ ISpyApplication::openUrlDialog(void)
 {
   QInputDialog dialog;
   dialog.setLabelText("Specify an ig-file url:");
-  dialog.setTextValue("http://iguana.web.cern.ch/iguana/ispy/igfiles/mc/electroweak/RelValZEE-1-2.ig");
+  dialog.setTextValue("http://iguana.web.cern.ch/iguana/ispy/igfiles/mc/electroweak/RelValZEE.ig");
   dialog.resize(430,72);
   // FIXME: use the latest file downloaded as default.
   if (!dialog.exec())
