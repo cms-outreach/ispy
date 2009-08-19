5735d4259ee35d4769b46d97eb0ef25f7189056a
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-08-19 14:22:27 +0200

    Build using verbose option.

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ source /afs/cern.ch/cms/IGUANA/externals/cmsset_default.sh
 scram p   IGUANA 200903191446-ge
 mv Iguana 200903191446-ge/src
 cd 200903191446-ge/src
-scram b -j5 ispy
+scram b -v -j5 ispy
