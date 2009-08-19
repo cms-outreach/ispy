16333915a6acfdad5cd5e430fb04de52c8882b25
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-08-19 14:09:41 +0200

    Fixes wrong path.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 SCRAM_ARCH=slc4_ia32_gcc432
-source /afs/cern.ch/cms/IGUANA/externals/slc4_ia32_gcc432/cmsset_default.sh
+source /afs/cern.ch/cms/IGUANA/externals/cmsset_default.sh
 scram p   IGUANA 200903191446-ge
 mv Iguana 200903191446-ge/src
 cd 200903191446-ge/src
