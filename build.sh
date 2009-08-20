ad7558d66acf45f8a5bc2280c345e1c8b8c7bbc9
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-08-20 11:51:26 +0200

    Copies files on afs when done.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sha -xe
 SCRAM_ARCH=slc4_ia32_gcc432
 source /afs/cern.ch/cms/IGUANA/externals/cmsset_default.sh
 scram p   IGUANA 200903191446-ge
@@ -6,3 +6,5 @@ mv Iguana 200903191446-ge/src
 mv Products 200903191446-ge/src
 cd 200903191446-ge/src
 scram b -v -j5 ispy
+strip ../lib/$SCRAM_ARCH/bin/ispy
+scp ../lib/$SCRAM_ARCH/bin/ispy eulisse@lxplus.cern.ch:/afs/cern.ch/user/i/iguana/www/ispy/bin/linux/ispy-next
