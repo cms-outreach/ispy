3f5d94ccbc6277f9a9d615f284f7e87c17d073b7
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-09-06 14:07:04 +0200

    Adapted to use qmake.

--- build.sh
+++ build.sh
@@ -1,10 +1,9 @@
-#!/bin/sha -xe
-SCRAM_ARCH=slc4_ia32_gcc432
-source /afs/cern.ch/cms/IGUANA/externals/cmsset_default.sh
-scram p   IGUANA 200903191446-ge
-mv Iguana 200903191446-ge/src
-mv Products 200903191446-ge/src
-cd 200903191446-ge/src
-scram b -v -j5 ispy
-strip ../bin/$SCRAM_ARCH/ispy
-scp ../bin/$SCRAM_ARCH/ispy lxplus.cern.ch:/afs/cern.ch/user/i/iguana/www/ispy/bin/linux/ispy-next
+#!/bin/sh -xe
+
+# Make sure that we are picking up the correct uname.
+export PATH=$PREFIX/bin:$PATH
+which qmake
+qmake
+make -j 2
+
+scp ./bin/ispy lxplus.cern.ch:/afs/cern.ch/user/i/eulisse/www/ispy-next-`uname`
