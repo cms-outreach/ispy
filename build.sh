88ef701de0a1c5c477af493c682360b934164da5
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-09-06 16:43:04 +0200

    Uses git to determine how to call the executable that gets uploaded.

--- build.sh
+++ build.sh
@@ -8,6 +8,17 @@ ln -sf Iguana/QtGUI/scripts/ispy.pro ispy.pro
 qmake
 make -j 2
 
+# In case there is some git repository information
+# we use it to make up a label for the result.
+# Otherwise the branch is always "next"
+branchname=next
+if [ -d .git ]
+then
+  branchname=`git symbolic-ref HEAD | sed -e's|refs/heads/||' | tr / -`
+fi
+
+
+
 case `uname` in
   Linux)
     scp ./ispy eulisse@lxplus.cern.ch:/afs/cern.ch/user/i/eulisse/www/ispy-next-`uname`-`uname m`
