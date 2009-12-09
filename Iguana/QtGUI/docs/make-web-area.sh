8eb049ce5eb0aec6c57b463256c2f325a3e73559
Giulio Eulisse <giulio.eulisse@cern.ch>
2009-12-09 17:16:22 +0100

    Documentation for 1.4.3 now in CVS.

---
+++ Iguana/QtGUI/docs/make-web-area.sh
@@ -0,0 +1,8 @@
+find . -type d -exec fs sa -dir {} -acl cern:nodes rl \;
+find . -type d -exec fs sa -dir {} -acl system:anyuser l \;
+find . -type d -exec fs sa -dir {} -acl lucas rlidwka \;
+find . -type d -exec fs sa -dir {} -acl eulisse rlidwka \;
+find . -type d -exec fs sa -dir {} -acl lat rlidwka \;
+find . -type d -exec fs sa -dir {} -acl mccauley rlidwka \;
+find . -type d -exec fs sa -dir {} -acl yana rlidwka \;
+find . -type d -exec fs sa -dir {} -acl muzaffar rlidwka \;
